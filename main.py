3ddfe430c61e02eb5eee900581919f536a2975de
Ilya <ikrav2003@mail.ru>
2023-06-15 21:43:22 +0300

    code style fix

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 def add(x, y):
     return x + y
 
+
 def subtract(x, y):
     return x - y
+
 
 def multiply(x, y):
     return x * y
 
+
 def divide(x, y):
     return x / y
+
 
 def exponentiate(x, y):
     return x ** y
 
+
 def remainder(x, y):
     return x % y
+
 
 print("Выберите операцию:")
 print("1. Сложение")
@@ -29,16 +35,16 @@ num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
 
 if choice == '1':
-    print(num1,"+",num2,"=", add(num1,num2))
+    print(num1, "+", num2, "=", add(num1, num2))
 elif choice == '2':
-    print(num1,"-",num2,"=", subtract(num1,num2))
+    print(num1, "-", num2, "=", subtract(num1, num2))
 elif choice == '3':
-    print(num1,"*",num2,"=", multiply(num1,num2))
+    print(num1, "*", num2, "=", multiply(num1, num2))
 elif choice == '4':
-    print(num1,"/",num2,"=", divide(num1,num2))
+    print(num1, "/", num2, "=", divide(num1, num2))
 elif choice == '5':
-    print(num1,"в степени",num2,"=", exponentiate(num1,num2))
+    print(num1, "в степени", num2, "=", exponentiate(num1, num2))
 elif choice == '6':
-    print("Остаток от деления",num1,"на",num2,"=", remainder(num1,num2))
+    print("Остаток от деления", num1, "на", num2, "=", remainder(num1, num2))
 else:
     print("Неверный ввод операции")
